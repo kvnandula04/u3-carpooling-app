050b40fa20bd5c1601c7422afc4d14653f7fe84b
fara <fzn21@bath.ac.uk>
2023-02-11 16:05:26 +0000

    Merge branch 'front-end' of https://github.com/kvnandula04/u3-carpooling-app into front-end

--- u3-carpooling-app/App.js
+++ u3-carpooling-app/App.js
@@ -1,12 +1,10 @@
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text } from "react-native";
-import { HomePage } from "./pages/HomePage";
 import LiveTripPage from "./pages/LiveTripPage";
 
 export default function App() {
     return (
         <SafeAreaView style={{ flex: 1 }}>
-            {/* <HomePage /> */}
             {/* <LiveTripPage /> */}
             <StatusBar style="auto" />
         </SafeAreaView>
