b596ffe9a16eb4a906df8e0f812665be4e42a6fb
Karthik Nandula <kvnandula04@gmail.com>
2023-02-12 10:56:49 +0000

    Cleaned up to return single signin page

--- u3-carpooling-app/App.js
+++ u3-carpooling-app/App.js
@@ -1,14 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text } from "react-native";
-import HomePage from "./pages/HomePage";
+import { StyleSheet } from "react-native";
+import SignupLoginPage from "./src/pages/SignupLoginPage";
 
 export default function App() {
-    return (
-        <SafeAreaView>
-            <HomePage />
-            <StatusBar style="auto" />
-        </SafeAreaView>
-    );
+  return <SignupLoginPage />;
 }
 
 const styles = StyleSheet.create({});
