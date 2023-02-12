67b0744a099872177a8a3e32e90329b892b3a4ee
Karthik Nandula <kvnandula04@gmail.com>
2023-02-12 19:08:32 +0000

    Sorted file paths

---
+++ u3-carpooling-app/src/components/LiveETA.js
@@ -0,0 +1,46 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const cream = "#F7F3EB";
+const blue = "#1774FF";
+const charcoal = "#3F3F3F";
+
+const LiveETA = (props) => {
+    return (
+        <Pressable style={[props.style, styles.frame]} onPress={props.onPress}>
+            <View style={[styles.card, styles.shadow]}></View>
+            <View style={[styles.card]}></View>
+            <Text style={[styles.text]}>eta.</Text>
+        </Pressable>
+    );
+};
+
+export default LiveETA;
+
+const styles = StyleSheet.create({
+    frame: {},
+    card: {
+        position: "absolute",
+        width: "100%",
+        height: "125%",
+        backgroundColor: blue,
+        borderColor: charcoal,
+        borderWidth: 5,
+        borderRadius: 32,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+    shadow: {
+        backgroundColor: "black",
+        borderColor: "black",
+        top: 6,
+        left: 6,
+    },
+    text: {
+        color: cream,
+        fontSize: 36,
+        position: "absolute",
+        bottom: 0,
+        left: 20,
+    },
+});
---
+++ u3-carpooling-app/src/components/LiveTripDuration.js
@@ -0,0 +1,45 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const cream = "#F7F3EB";
+const charcoal = "#3F3F3F";
+
+const LiveTripDuration = (props) => {
+    return (
+        <Pressable style={[props.style, styles.frame]} onPress={props.onPress}>
+            <View style={[styles.card, styles.shadow]}></View>
+            <View style={[styles.card]}></View>
+            <Text style={[styles.text]}>trip length.</Text>
+        </Pressable>
+    );
+};
+
+export default LiveTripDuration;
+
+const styles = StyleSheet.create({
+    frame: {},
+    card: {
+        position: "absolute",
+        width: "100%",
+        height: "125%",
+        backgroundColor: cream,
+        borderColor: charcoal,
+        borderWidth: 5,
+        borderRadius: 32,
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+    shadow: {
+        backgroundColor: "black",
+        borderColor: "black",
+        top: 6,
+        left: 6,
+    },
+    text: {
+        color: charcoal,
+        fontSize: 36,
+        position: "absolute",
+        bottom: 10,
+        left: 20,
+    },
+});
