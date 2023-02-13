055d22ca56c83e0dd9f4d1e0b4ac08d306b259af
Sam Worthington <sjw233@bath.ac.uk>
2023-02-13 12:44:35 +0000

    Opening the Bath signin page

--- u3-carpooling-app/pages/VerificationPage.js
+++ u3-carpooling-app/pages/VerificationPage.js
@@ -1,72 +1,32 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, Alert, Linking } from "react-native";
 
-const App = () => {
-  const [email, setEmail] = useState('');
-  const [status, setStatus] = useState('Email not verified');
-
-  const handleSubmit = () => {
-    if (!email.endsWith('@bath.ac.uk')) {
-      setStatus('Invalid email');
-      return;
-    }
-
-    setStatus('Email verification link sent');
-    // code to send email verification link here
-
-    // once the user clicks the verification link, call this function
-    // to update the status
-    const handleVerificationSuccess = () => {
-      setStatus('Your email is verified');
-    };
-  };
+const VerificationPage = () => {
+  const [emailVerified, setEmailVerified] = useState(false);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.statusText}>{status}</Text>
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
-        <Text style={styles.buttonText}>Verify</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 18 }}>
+        {emailVerified ? "Email Verified" : "Email Not Verified"}
+      </Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "blue",
+          padding: 10,
+          marginTop: 20,
+          width: "80%",
+        }}
+
+        onPress={() => Linking.openURL("https://auth.bath.ac.uk/login")}
+      >
+        <Text style={{ color: "white", textAlign: "center" }}>
+          Verify Email
+        </Text>
       </TouchableOpacity>
+
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  statusText: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  input: {
-    width: 200,
-    height: 44,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
-    marginBottom: 10,
-  },
-  button: {
-    width: 100,
-    height: 44,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: 'white',
-  },
-});
-
-export default App;
+export default VerificationPage;
