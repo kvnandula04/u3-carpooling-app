7d7d34b7034b48cb7d275278c53949a919d2847f
fara <fzn21@bath.ac.uk>
2023-02-14 12:37:55 +0000

    Changed Map to flex Finished Arrived barebones

--- u3-carpooling-app/components/LiveMap.js
+++ u3-carpooling-app/components/LiveMap.js
@@ -8,11 +8,11 @@ const greenShadow = "#278A17";
 const LiveMap = (props) => {
     return (
         <Pressable style={[props.style, styles.frame]} onPress={props.onPress}>
-            <View style={[styles.card, styles.shadow]}></View>
             <View style={[styles.card]}>
-                <MapView style={[styles.card, styles.map]} />
+                <MapView style={[styles.map]} mapType="mutedStandard" />
+                <Text style={[styles.text]}>U3</Text>
             </View>
-            <Text style={[styles.text]}>U3</Text>
+            <View style={[styles.card, styles.shadow]}></View>
         </Pressable>
     );
 };
@@ -21,30 +21,32 @@ export default LiveMap;
 
 const styles = StyleSheet.create({
     frame: {},
-    map: {},
-    card: {
-        position: "absolute",
+    map: {
         width: "100%",
-        height: "125%",
-        backgroundColor: "white",
+        height: "100%",
+        marginBottom: 25,
+    },
+    card: {
+        width: "90%",
+        height: "80%",
         borderColor: green,
         borderWidth: 5,
         borderRadius: 32,
-        borderBottomLeftRadius: 0,
-        borderBottomRightRadius: 0,
         overflow: "hidden",
+        marginBottom: 5,
     },
     shadow: {
+        zIndex: -1,
         backgroundColor: greenShadow,
         borderColor: greenShadow,
-        top: 6,
-        left: 6,
+        marginTop: -214,
+        marginRight: -12,
     },
     text: {
         color: "black",
         fontSize: 36,
         position: "absolute",
-        top: 10,
-        right: 20,
+        top: 5,
+        right: 10,
     },
 });
--- u3-carpooling-app/pages/Arrived.js
+++ u3-carpooling-app/pages/Arrived.js
@@ -1,6 +1,7 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import LiveMap from "../components/LiveMap";
+import Svg, { SvgProps, G, Path, Defs } from "react-native-svg";
 
 const cream = "#F7F3EB";
 const green = "#218C4C";
@@ -11,22 +12,25 @@ const Arrived = () => {
     const onPressQR = () => {
         console.log("Take to QR Scanner");
     };
+    const onPressMap = () => {
+        console.log("Take to Map");
+    };
 
     return (
         <View id="pageFrame" style={styles.pageFrame}>
             <View id="mapFrame" style={styles.mapFrame}>
-                <LiveMap id="map" style={styles.ma} p />
+                <LiveMap id="map" style={styles.map} onPress={onPressMap} />
             </View>
             <View id="titleFrame" style={styles.titleFrame}>
                 <Text id="title" style={styles.title}>
                     You've{"\n"}Arrived!
                 </Text>
-                {/* <Text
+                <Text
                     id="titleShadow"
                     style={[styles.title, styles.titleShadow]}
                 >
                     You've{"\n"}Arrived!
-                </Text> */}
+                </Text>
                 <Text id="subTitle" style={styles.subTitle}>
                     Confirm your trip is complete by...
                 </Text>
@@ -34,10 +38,31 @@ const Arrived = () => {
             <View id="qrFrame" style={styles.qrFrame}>
                 <Pressable id="qr" style={styles.qr} onPress={onPressQR}>
                     <Text id="qrText" style={styles.qrText}>
-                        scan{"\n"}Driver's{"\n"}QR{"\n"}code
+                        scan{"\n"}Driver's{"\n"}QR{"\n"}code.
                     </Text>
+                    <Svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width={88}
+                        height={88}
+                        fill="none"
+                        // {...props}
+                        style={{
+                            marginLeft: -35,
+                            marginBottom: 4,
+                        }}
+                    >
+                        <G filter="url(#a)">
+                            <Path
+                                stroke="#272727"
+                                strokeLinecap="square"
+                                strokeWidth={5}
+                                d="M31.17 8.5H16.06a7.56 7.56 0 0 0-7.56 7.56v15.1M31.17 76.5H16.06a7.56 7.56 0 0 1-7.56-7.56v-15.1M53.83 8.5h15.11a7.56 7.56 0 0 1 7.56 7.56v15.1m0 22.67v15.11a7.56 7.56 0 0 1-7.56 7.56h-15.1M8.5 42.5h68"
+                            />
+                        </G>
+                        <Defs></Defs>
+                    </Svg>
                 </Pressable>
-                {/* <View id="qrShadow" style={[styles.qr, styles.qrShadow]}></View> */}
+                <View id="qrShadow" style={[styles.qr, styles.qrShadow]}></View>
             </View>
         </View>
     );
@@ -50,62 +75,70 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: black,
     },
     mapFrame: {
         flex: 2,
         width: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "red",
+        // backgroundColor: "red",
     },
     map: {
         flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
     },
     titleFrame: {
         flex: 1,
         width: "100%",
         justifyContent: "flex-end",
         alignItems: "center",
-        backgroundColor: "blue",
+        // backgroundColor: "blue",
     },
     title: {
-        zIndex: 1,
         fontSize: 48,
         color: cream,
         textAlign: "center",
     },
     titleShadow: {
-        zIndex: 0,
+        zIndex: -1,
         color: green,
+        marginTop: -114,
+        marginRight: -8,
     },
     subTitle: {
         fontSize: 24,
         color: cream,
         textAlign: "center",
+        marginTop: -10,
     },
     qrFrame: {
         flex: 3,
         width: "100%",
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "green",
+        // backgroundColor: "green",
     },
     qr: {
-        zIndex: 1,
+        zIndex: 0,
         height: "60%",
         width: "60%",
         backgroundColor: cream,
         borderColor: charcoal,
         borderWidth: 5,
         borderRadius: 32,
+        flexDirection: "row",
+        alignItems: "flex-end",
     },
     qrShadow: {
-        zIndex: 0,
+        zIndex: -1,
         backgroundColor: "black",
         borderColor: "black",
+        marginTop: -234,
+        marginRight: -12,
     },
     qrText: {
         color: black,
         fontSize: 48,
+        width: "75%",
     },
 });
