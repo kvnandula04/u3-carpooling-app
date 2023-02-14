224f7f88cf5dcfa17daab149175c9b584d4d96a6
Sam Worthington <sjw233@bath.ac.uk>
2023-02-14 16:05:41 +0000

    Most recent verification page version

--- u3-carpooling-app/pages/VerificationPage.js
+++ u3-carpooling-app/pages/VerificationPage.js
@@ -1,32 +1,93 @@
 import React, { useState } from "react";
-import { View, Text, TextInput, TouchableOpacity, Alert, Linking } from "react-native";
+import { View, Text, Button} from "react-native";
+import { WebView } from 'react-native-webview';
 
 const VerificationPage = () => {
-  const [emailVerified, setEmailVerified] = useState(false);
+    const [isEmailVerified, setIsEmailVerified] = useState(false);
+    const [showWebView, setShowWebView] = useState(false);
+    const [showTextAndButton, setShowTextAndButton] = useState(true);
 
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ fontSize: 18 }}>
-        {emailVerified ? "Email Verified" : "Email Not Verified"}
-      </Text>
+    const verifyEmail = () => {
+        setShowTextAndButton(false);
+        setShowWebView(true);
+    };
 
-      <TouchableOpacity
-        style={{
-          backgroundColor: "blue",
-          padding: 10,
-          marginTop: 20,
-          width: "80%",
-        }}
+    const onNavigationStateChange = (navState) => {
+        if (navState.url === 'https://moodle.bath.ac.uk/') {
+            setIsEmailVerified(true);
+            setShowWebView(false);
+            setShowTextAndButton(true);
+        }
+    };
 
-        onPress={() => Linking.openURL("https://auth.bath.ac.uk/login")}
-      >
-        <Text style={{ color: "white", textAlign: "center" }}>
-          Verify Email
-        </Text>
-      </TouchableOpacity>
-
-    </View>
-  );
-};
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            {isEmailVerified ? (
+            <Text>Email verified</Text>
+            ) : (
+            <>
+                {showTextAndButton && (
+                <>
+                    <Text>Email is not verified</Text>
+                    <Button title="Press to verify email" onPress={verifyEmail} />
+                </>
+                )}
+                {showWebView && (
+                    <WebView
+                        source={{
+                            uri: 'https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1',
+                        }}
+                        onNavigationStateChange={onNavigationStateChange}
+                    />
+                )}
+            </>
+        )}
+        </View>
+    );
+}
 
 export default VerificationPage;
+
+
+//-----------------------This is the code that works-----------------------
+// export default class VerificationPage extends Component{
+
+//     render(){
+//         return <WebView source = {{uri: 'https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1'}} />
+//     }
+// }
+
+
+//-----------------------This is the code that doesn't work-----------------------
+// const VerificationPage = () => {
+//   const [emailVerified, setEmailVerified] = useState(false);
+
+//   const handleVerify = () => {
+//     setEmailVerified(true);
+//   };
+
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       {emailVerified ? (
+//         <Text>Email Verified</Text>
+//       ) : (
+//         <>
+//           <Text>Email Not Verified</Text>
+//           <Button title="Verify Email" onPress={handleVerify} />
+//         </>
+//       )}
+//       {!emailVerified && (
+//         <WebView
+//           source={{ uri: 'https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1' }}
+//           onNavigationStateChange={(event) => {
+//             if (event.url === 'https://moodle.bath.ac.uk/') {
+//               handleVerify();
+//             }
+//           }}
+//         />
+//       )}
+//     </View>
+//   );
+// };
+
+// export default VerificationPage;
