86598047909eb5f202c6b8d4fd22c3b8b0ed9cda
Sam Worthington <sjw233@bath.ac.uk>
2023-02-16 17:32:25 +0000

    Completed Version

--- u3-carpooling-app/pages/VerificationPage.js
+++ u3-carpooling-app/pages/VerificationPage.js
@@ -2,17 +2,6 @@ import React, { useState } from "react";
 import { View, Text, Button} from "react-native";
 import { WebView } from 'react-native-webview';
 
-
-// const VerificationPage = () => {
-    // return(
-    //     <View style = {{flex: 1}}>
-    //         <Text>Verification Page</Text>
-    //         <Button title = "Verify Email" onPress = {() => {}} />
-    //         <WebView source = {{uri: 'https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1'}} />
-    //     </View>
-    // );
-// }
-
 const VerificationPage = () => {
     const [isEmailVerified, setIsEmailVerified] = useState(false);
     const [showWebView, setShowWebView] = useState(false);
@@ -58,47 +47,3 @@ const VerificationPage = () => {
 }
 
 export default VerificationPage;
-
-
-//-----------------------This is the code that works-----------------------
-// export default class VerificationPage extends Component{
-
-//     render(){
-//         return <WebView source = {{uri: 'https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1'}} />
-//     }
-// }
-
-
-//-----------------------This is the code that doesn't work-----------------------
-// const VerificationPage = () => {
-//   const [emailVerified, setEmailVerified] = useState(false);
-
-//   const handleVerify = () => {
-//     setEmailVerified(true);
-//   };
-
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//       {emailVerified ? (
-//         <Text>Email Verified</Text>
-//       ) : (
-//         <>
-//           <Text>Email Not Verified</Text>
-//           <Button title="Verify Email" onPress={handleVerify} />
-//         </>
-//       )}
-//       {!emailVerified && (
-//         <WebView
-//           source={{ uri: 'https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1' }}
-//           onNavigationStateChange={(event) => {
-//             if (event.url === 'https://moodle.bath.ac.uk/') {
-//               handleVerify();
-//             }
-//           }}
-//         />
-//       )}
-//     </View>
-//   );
-// };
-
-// export default VerificationPage;
