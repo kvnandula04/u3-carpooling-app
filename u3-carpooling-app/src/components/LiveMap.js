21e3497fdb468ff9cbcaa92fc78e2666726f8bb8
Karthik Nandula <kvnandula04@gmail.com>
2023-02-18 22:52:28 +0000

    Modified for reusability

--- u3-carpooling-app/src/components/LiveMap.js
+++ u3-carpooling-app/src/components/LiveMap.js
@@ -6,38 +6,35 @@ const green = "#4CD835";
 const greenShadow = "#278A17";
 
 const LiveMap = (props) => {
-    return (
-        <Pressable
-            id="frame"
-            style={[props.style, styles.frame]}
-            onPress={props.onPress}
-        >
-            <View style={[props.cardStyle]}>
-                <MapView style={[styles.map]} mapType="mutedStandard" />
-                <Text style={[styles.text]}>U3</Text>
-            </View>
-            <View
-                id="shadow"
-                style={[props.cardStyle, props.shadowStyle]}
-            ></View>
-        </Pressable>
-    );
+  return (
+    <Pressable
+      id="frame"
+      style={[props.style, styles.frame]}
+      onPress={props.onPress}
+    >
+      <View style={[props.cardStyle]}>
+        <MapView style={[styles.map]} mapType="mutedStandard" />
+        <Text style={[styles.text]}>{props.text}</Text>
+      </View>
+      <View id="shadow" style={[props.cardStyle, props.shadowStyle]}></View>
+    </Pressable>
+  );
 };
 
 export default LiveMap;
 
 const styles = StyleSheet.create({
-    frame: {},
-    map: {
-        width: "100%",
-        height: "100%",
-        // marginBottom: 25,
-    },
-    text: {
-        color: "black",
-        fontSize: 36,
-        position: "absolute",
-        top: 5,
-        right: 10,
-    },
+  frame: {},
+  map: {
+    width: "100%",
+    height: "100%",
+    // marginBottom: 25,
+  },
+  text: {
+    color: "black",
+    fontSize: 36,
+    position: "absolute",
+    top: 5,
+    right: 10,
+  },
 });
