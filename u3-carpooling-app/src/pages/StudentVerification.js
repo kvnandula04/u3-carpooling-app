5acd23c37100a339d17d7d7f35d7b37ae14c58c1
Karthik Nandula <kvnandula04@gmail.com>
2023-02-23 10:05:50 +0000

    Integrated student sign-in

--- u3-carpooling-app/src/pages/StudentVerification.js
+++ u3-carpooling-app/src/pages/StudentVerification.js
@@ -1,54 +1,105 @@
-import React from "react";
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Pressable,
+  Button,
+  SafeAreaView,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import Logo from "../components/Logo";
 import GridBackground from "../../assets/grid-background";
+import { WebView } from "react-native-webview";
 
 export default function StudentVerification() {
   const navigation = useNavigation();
-  const onSigninPressed = () => {
+  const onSignedIn = () => {
     navigation.navigate("DriverVerification");
+  };
+
+  const [isEmailVerified, setIsEmailVerified] = useState(false);
+  const [showWebView, setShowWebView] = useState(false);
+  const [showView, setShowView] = useState(true);
+
+  const verifyEmail = () => {
+    setShowView(false);
+    setShowWebView(true);
+  };
+
+  const onNavigationStateChange = (navState) => {
+    if (navState.url === "https://moodle.bath.ac.uk/") {
+      setIsEmailVerified(true);
+      setShowView(true);
+      setShowWebView(false);
+    }
   };
 
   return (
     <View style={styles.container}>
-      <GridBackground
-        position="absolute"
-        zIndex={-5}
-        lineColor={"black"}
-        style={{ backgroundColor: "#1daf59" }}
-      />
-
-      <View style={styles.flex1}>
-        <Logo fontSize={40} color={"#f7f3eb"} marginTop={"15%"} />
-      </View>
-
-      <View style={styles.flex2}>
-        <Text style={styles.heading}>Student{"\n"}Verification</Text>
-      </View>
-
-      <View style={styles.flex3}>
-        <View id="signinFrame" style={styles.signinFrame}>
-          <Pressable
-            id="signinButton"
-            style={styles.signinButton}
-            onPress={onSigninPressed}
-          >
-            <Text style={styles.text}>UoB Sign In</Text>
-          </Pressable>
-          <View
-            id="signinButtonShadow"
-            style={[styles.signinButton, styles.signinButtonShadow]}
-          ></View>
+      {isEmailVerified ? (
+        <View style={[styles.inner, { flexDirection: "column" }]}>
+          <Text style={{ fontSize: 30 }}>Email verified</Text>
+          <Button title="Continue" onPress={onSignedIn} />
         </View>
-      </View>
+      ) : (
+        <View style={styles.container}>
+          {showView && (
+            <>
+              <GridBackground
+                position="absolute"
+                zIndex={-5}
+                lineColor={"black"}
+                style={{ backgroundColor: "#1daf59" }}
+              />
 
-      <View style={styles.flex4}>
-        <View style={styles.circle1}></View>
-        <Pressable onPress={() => navigation.navigate("DriverVerification")}>
-          <View style={styles.circle2}></View>
-        </Pressable>
-      </View>
+              <View style={styles.flex1}>
+                <Logo fontSize={40} color={"#f7f3eb"} marginTop={"15%"} />
+              </View>
+
+              <View style={styles.flex2}>
+                <Text style={styles.heading}>Student{"\n"}Verification</Text>
+              </View>
+
+              <View style={styles.flex3}>
+                <View id="signinFrame" style={styles.signinFrame}>
+                  <Pressable
+                    id="signinButton"
+                    style={styles.signinButton}
+                    onPress={verifyEmail}
+                  >
+                    <Text style={styles.text}>UoB Sign In</Text>
+                  </Pressable>
+                  <View
+                    id="signinButtonShadow"
+                    style={[styles.signinButton, styles.signinButtonShadow]}
+                  ></View>
+                </View>
+              </View>
+
+              <View style={styles.flex4}>
+                <View style={styles.circle1}></View>
+                <Pressable
+                  onPress={() => navigation.navigate("DriverVerification")}
+                >
+                  <View style={styles.circle2}></View>
+                </Pressable>
+              </View>
+            </>
+          )}
+          {showWebView && (
+            <SafeAreaView style={styles.inner}>
+              <WebView
+                style={{ flex: 1 }}
+                source={{
+                  uri: "https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1",
+                }}
+                onNavigationStateChange={onNavigationStateChange}
+              />
+            </SafeAreaView>
+          )}
+        </View>
+      )}
     </View>
   );
 }
@@ -56,6 +107,12 @@ export default function StudentVerification() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  inner: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   flex1: {
     flex: 2,
