b17bc8c928ab1e7d5ad7e3f2e86d8a34f20339dc
fara <fzn21@bath.ac.uk>
2023-03-08 23:55:03 +0000

    BottomSheet android & keyboard fix

--- u3-carpooling-app/App.js
+++ u3-carpooling-app/App.js
@@ -1,6 +1,11 @@
 import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
-    return <Navigation />;
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Navigation />
+        </GestureHandlerRootView>
+    );
 }
