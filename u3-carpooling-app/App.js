d732619beafd5b16b450bd3fbd4dc7af67312ebc
fara <fzn21@bath.ac.uk>
2023-03-08 17:57:24 +0000

    PlanTrip page killed

--- u3-carpooling-app/App.js
+++ u3-carpooling-app/App.js
@@ -2,5 +2,5 @@ import { StyleSheet } from "react-native";
 import Navigation from "./src/navigation/Navigation";
 
 export default function App() {
-  return <Navigation />;
+    return <Navigation />;
 }
