5ceab0f067fab7b7cf1d5d3fda56d58997f17848
fara <fzn21@bath.ac.uk>
2023-03-08 16:03:12 +0000

    OldHomePage + PlanTrip clean

--- u3-carpooling-app/src/components/Logo.js
+++ u3-carpooling-app/src/components/Logo.js
@@ -2,38 +2,38 @@ import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 
 const Logo = (props) => {
-  return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          color: props.color,
-          fontSize: props.fontSize,
-          lineHeight: 0,
-          fontFamily: "syne",
-          marginTop: props.marginTop,
-        }}
-      >
-        U
-      </Text>
-      <Text
-        style={{
-          color: props.color,
-          fontSize: props.fontSize,
-          lineHeight: -10,
-          fontFamily: "syne",
-          marginTop: props.marginTop,
-        }}
-      >
-        3
-      </Text>
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text
+                style={{
+                    color: props.color,
+                    fontSize: props.fontSize,
+                    lineHeight: 0,
+                    fontFamily: "syne",
+                    marginTop: props.marginTop,
+                }}
+            >
+                U
+            </Text>
+            <Text
+                style={{
+                    color: props.color,
+                    fontSize: props.fontSize,
+                    lineHeight: -10,
+                    fontFamily: "syne",
+                    marginTop: props.marginTop,
+                }}
+            >
+                3
+            </Text>
+        </View>
+    );
 };
 
 export default Logo;
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-  },
+    container: {
+        flexDirection: "row",
+    },
 });
