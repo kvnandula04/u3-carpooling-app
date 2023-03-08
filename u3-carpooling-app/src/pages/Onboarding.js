753525c72d292a7ed65a9882443a86f91b2dfca5
fara <fzn21@bath.ac.uk>
2023-03-08 20:33:18 +0000

    samesh2

--- u3-carpooling-app/src/pages/Onboarding.js
+++ u3-carpooling-app/src/pages/Onboarding.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useCallback, useState } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import {
-  Text,
-  View,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
+    Text,
+    View,
+    StyleSheet,
+    TextInput,
+    TouchableOpacity,
 } from "react-native";
 import Logo from "../components/Logo";
 import useFonts from "../hooks/UseFonts";
@@ -13,214 +13,219 @@ import GridBackground from "../../assets/grid-background";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Onboarding() {
-  const [IsReady, SetIsReady] = useState(false);
-  const navigation = useNavigation();
-  const onContinuePressed = () => {
-    navigation.navigate("HomePage");
-  };
-  const [userName, setUserName] = React.useState("");
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
+    const [IsReady, SetIsReady] = useState(false);
+    const navigation = useNavigation();
+    const onContinuePressed = () => {
+        navigation.navigate("OldHomePage");
+    };
+    const [userName, setUserName] = React.useState("");
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
 
-  // Loading in fonts
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await useFonts();
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        SetIsReady(true);
-      }
+    // Loading in fonts
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await useFonts();
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                SetIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (IsReady) {
+            await SplashScreen.hideAsync();
+        }
+    }, [IsReady]);
+
+    if (!IsReady) {
+        return null;
     }
 
-    prepare();
-  }, []);
+    return (
+        <View style={styles.container}>
+            <GridBackground
+                position="absolute"
+                zIndex={-5}
+                lineColor={"black"}
+                style={{ backgroundColor: "#e7ada0" }}
+            />
 
-  const onLayoutRootView = useCallback(async () => {
-    if (IsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [IsReady]);
+            <View style={styles.flex1}>
+                <Logo fontSize={40} color={"#000"} marginTop={"15%"} />
+            </View>
 
-  if (!IsReady) {
-    return null;
-  }
+            <View style={styles.flex2}>
+                <Text style={styles.heading}>The Boring{"\n"}Stuff...</Text>
+            </View>
 
-  return (
-    <View style={styles.container}>
-      <GridBackground
-        position="absolute"
-        zIndex={-5}
-        lineColor={"black"}
-        style={{ backgroundColor: "#e7ada0" }}
-      />
+            <View style={styles.flex3}>
+                <Text style={styles.subheading}> Enter your details: </Text>
+                <View id="userNameFrame" style={styles.frame}>
+                    <View id="userNameButton" style={styles.button}>
+                        <TextInput
+                            style={styles.text}
+                            placeholder="Bath Username"
+                            value={userName}
+                            onChangeText={(userName) => setUserName(userName)}
+                        ></TextInput>
+                    </View>
+                    <View
+                        id="userNameShadow"
+                        style={[styles.button, styles.shadow]}
+                    ></View>
+                </View>
+                <View id="firstNameFrame" style={styles.frame}>
+                    <View id="firstNameButton" style={styles.button}>
+                        <TextInput
+                            style={styles.text}
+                            placeholder="First Name"
+                            value={firstName}
+                            onChangeText={(firstName) =>
+                                setFirstName(firstName)
+                            }
+                        ></TextInput>
+                    </View>
+                    <View
+                        id="firstNameShadow"
+                        style={[styles.button, styles.shadow]}
+                    ></View>
+                </View>
+                <View id="lastNameFrame" style={styles.frame}>
+                    <View id="lastNameButton" style={styles.button}>
+                        <TextInput
+                            style={styles.text}
+                            placeholder="Last Name"
+                            value={lastName}
+                            onChangeText={(lastName) => setLastName(lastName)}
+                        ></TextInput>
+                    </View>
+                    <View
+                        id="lastNameShadow"
+                        style={[styles.button, styles.shadow]}
+                    ></View>
+                </View>
+            </View>
 
-      <View style={styles.flex1}>
-        <Logo fontSize={40} color={"#000"} marginTop={"15%"} />
-      </View>
+            {/* Select between driver and passenger */}
+            <View style={styles.flex4}></View>
 
-      <View style={styles.flex2}>
-        <Text style={styles.heading}>The Boring{"\n"}Stuff...</Text>
-      </View>
-
-      <View style={styles.flex3}>
-        <Text style={styles.subheading}> Enter your details: </Text>
-        <View id="userNameFrame" style={styles.frame}>
-          <View id="userNameButton" style={styles.button}>
-            <TextInput
-              style={styles.text}
-              placeholder="Bath Username"
-              value={userName}
-              onChangeText={(userName) => setUserName(userName)}
-            ></TextInput>
-          </View>
-          <View
-            id="userNameShadow"
-            style={[styles.button, styles.shadow]}
-          ></View>
+            <View style={styles.flex5}>
+                <TouchableOpacity
+                    style={styles.button2}
+                    onPress={onContinuePressed}
+                >
+                    <Text style={styles.text}>continue.</Text>
+                </TouchableOpacity>
+            </View>
         </View>
-        <View id="firstNameFrame" style={styles.frame}>
-          <View id="firstNameButton" style={styles.button}>
-            <TextInput
-              style={styles.text}
-              placeholder="First Name"
-              value={firstName}
-              onChangeText={(firstName) => setFirstName(firstName)}
-            ></TextInput>
-          </View>
-          <View
-            id="firstNameShadow"
-            style={[styles.button, styles.shadow]}
-          ></View>
-        </View>
-        <View id="lastNameFrame" style={styles.frame}>
-          <View id="lastNameButton" style={styles.button}>
-            <TextInput
-              style={styles.text}
-              placeholder="Last Name"
-              value={lastName}
-              onChangeText={(lastName) => setLastName(lastName)}
-            ></TextInput>
-          </View>
-          <View
-            id="lastNameShadow"
-            style={[styles.button, styles.shadow]}
-          ></View>
-        </View>
-      </View>
-
-      {/* Select between driver and passenger */}
-      <View style={styles.flex4}></View>
-
-      <View style={styles.flex5}>
-        <TouchableOpacity style={styles.button2} onPress={onContinuePressed}>
-          <Text style={styles.text}>continue.</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  flex1: {
-    flex: 2.75,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  flex2: {
-    flex: 1.5,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  flex3: {
-    flex: 3,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "5%",
-  },
-  flex4: {
-    flex: 2,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  flex5: {
-    flex: 0.5,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "flex-end",
-    paddingBottom: "15%",
-  },
-  heading: {
-    fontFamily: "atkinson-italic",
-    fontSize: 54,
-    color: "#5dada0",
-    lineHeight: 53,
-    shadowColor: "#000",
-    shadowOpacity: 1,
-    shadowRadius: 2,
-    shadowOffset: {
-      height: 4,
-      width: 4,
+    container: {
+        flex: 1,
     },
-    textAlign: "center",
-  },
-  subheading: {
-    fontFamily: "atkinson-italic",
-    fontSize: 24,
-    color: "#000",
-    lineHeight: 53,
-    textAlign: "center",
-  },
-  frame: {
-    width: "60%",
-    height: "25%",
-    marginTop: "5%",
-    // backgroundColor: "white",
-  },
-  button: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 32,
-    borderColor: "#000",
-    borderWidth: 5,
-    backgroundColor: "#beddd6",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  shadow: {
-    zIndex: -1,
-    position: "absolute",
-    backgroundColor: "#000",
-    top: 6,
-    left: 6,
-  },
-  text: {
-    fontSize: 24,
-    fontFamily: "atkinson-italic",
-    color: "#000",
-  },
-  button2: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  circle1: {
-    width: 30,
-    height: 30,
-    borderRadius: 1000,
-    backgroundColor: "#000000",
-    marginRight: "1%",
-  },
-  circle2: {
-    width: 30,
-    height: 30,
-    borderRadius: 1000,
-    borderWidth: 6,
-    borderColor: "#000000",
-    backgroundColor: "#ffb800",
-    marginLeft: "1%",
-  },
+    flex1: {
+        flex: 2.75,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    flex2: {
+        flex: 1.5,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    flex3: {
+        flex: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "5%",
+    },
+    flex4: {
+        flex: 2,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    flex5: {
+        flex: 0.5,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "flex-end",
+        paddingBottom: "15%",
+    },
+    heading: {
+        fontFamily: "atkinson-italic",
+        fontSize: 54,
+        color: "#5dada0",
+        lineHeight: 53,
+        shadowColor: "#000",
+        shadowOpacity: 1,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 4,
+            width: 4,
+        },
+        textAlign: "center",
+    },
+    subheading: {
+        fontFamily: "atkinson-italic",
+        fontSize: 24,
+        color: "#000",
+        lineHeight: 53,
+        textAlign: "center",
+    },
+    frame: {
+        width: "60%",
+        height: "25%",
+        marginTop: "5%",
+        // backgroundColor: "white",
+    },
+    button: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 32,
+        borderColor: "#000",
+        borderWidth: 5,
+        backgroundColor: "#beddd6",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    shadow: {
+        zIndex: -1,
+        position: "absolute",
+        backgroundColor: "#000",
+        top: 6,
+        left: 6,
+    },
+    text: {
+        fontSize: 24,
+        fontFamily: "atkinson-italic",
+        color: "#000",
+    },
+    button2: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    circle1: {
+        width: 30,
+        height: 30,
+        borderRadius: 1000,
+        backgroundColor: "#000000",
+        marginRight: "1%",
+    },
+    circle2: {
+        width: 30,
+        height: 30,
+        borderRadius: 1000,
+        borderWidth: 6,
+        borderColor: "#000000",
+        backgroundColor: "#ffb800",
+        marginLeft: "1%",
+    },
 });
--- u3-carpooling-app/src/pages/SignupLoginPage.js
+++ u3-carpooling-app/src/pages/SignupLoginPage.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useCallback } from "react";
 import * as SplashScreen from "expo-splash-screen";
 import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Pressable,
-  SafeAreaView,
-  Button,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Pressable,
+    SafeAreaView,
+    Button,
 } from "react-native";
 import Logo from "../components/Logo";
 import useFonts from "../hooks/UseFonts";
@@ -16,193 +16,198 @@ import { useNavigation } from "@react-navigation/native";
 import { WebView } from "react-native-webview";
 
 export default function SignupLoginPage() {
-  const [IsReady, SetIsReady] = useState(false);
-  const navigation = useNavigation();
-  const onSignupPressed = () => {
-    navigation.navigate("StudentVerification");
-  };
+    const [IsReady, SetIsReady] = useState(false);
+    const navigation = useNavigation();
+    const onSignupPressed = () => {
+        navigation.navigate("StudentVerification");
+    };
 
-  const onLoginPressed = () => {
-    navigation.navigate("HomePage");
-  };
+    const onLoginPressed = () => {
+        navigation.navigate("OldHomePage");
+    };
 
-  const [isEmailVerified, setIsEmailVerified] = useState(false);
-  const [showWebView, setShowWebView] = useState(false);
-  const [showView, setShowView] = useState(true);
+    const [isEmailVerified, setIsEmailVerified] = useState(false);
+    const [showWebView, setShowWebView] = useState(false);
+    const [showView, setShowView] = useState(true);
 
-  const verifyEmail = () => {
-    setShowView(false);
-    setShowWebView(true);
-  };
+    const verifyEmail = () => {
+        setShowView(false);
+        setShowWebView(true);
+    };
 
-  const onNavigationStateChange = (navState) => {
-    if (navState.url === "https://moodle.bath.ac.uk/") {
-      setIsEmailVerified(true);
-      setShowView(true);
-      setShowWebView(false);
+    const onNavigationStateChange = (navState) => {
+        if (navState.url === "https://moodle.bath.ac.uk/") {
+            setIsEmailVerified(true);
+            setShowView(true);
+            setShowWebView(false);
+        }
+    };
+
+    // Loading in fonts
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await useFonts();
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                SetIsReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (IsReady) {
+            await SplashScreen.hideAsync();
+        }
+    }, [IsReady]);
+
+    if (!IsReady) {
+        return null;
     }
-  };
 
-  // Loading in fonts
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await useFonts();
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        SetIsReady(true);
-      }
-    }
-
-    prepare();
-  }, []);
-
-  const onLayoutRootView = useCallback(async () => {
-    if (IsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [IsReady]);
-
-  if (!IsReady) {
-    return null;
-  }
-
-  return (
-    <View style={styles.container}>
-      {isEmailVerified ? (
-        <View style={[styles.inner, { flexDirection: "column" }]}>
-          <Text style={{ fontSize: 30 }}>Email verified</Text>
-          <Button title="Continue" onPress={onLoginPressed} />
-        </View>
-      ) : (
+    return (
         <View style={styles.container}>
-          {showView && (
-            <>
-              <GridBackground
-                position="absolute"
-                zIndex={-5}
-                lineColor={"black"}
-                style={{ backgroundColor: "#f7f3eb" }}
-              />
-
-              <View id style={styles.flex1}>
-                <Logo fontSize={86} marginTop={"15%"} />
-              </View>
-
-              <View id="headingAndJoin" style={styles.flex2}>
-                <Text id="headingText" style={styles.heading}>
-                  Student{"\n"}Car Pooling
-                </Text>
-                <View id="joinFrame" style={styles.joinFrame}>
-                  <Pressable
-                    id="joinButton"
-                    style={styles.joinButton}
-                    onPress={onSignupPressed}
-                  >
-                    <Text style={styles.text}>Join.</Text>
-                  </Pressable>
-                  <View
-                    id="joinButtonShadow"
-                    style={[styles.joinButton, styles.joinButtonShadow]}
-                  ></View>
+            {isEmailVerified ? (
+                <View style={[styles.inner, { flexDirection: "column" }]}>
+                    <Text style={{ fontSize: 30 }}>Email verified</Text>
+                    <Button title="Continue" onPress={onLoginPressed} />
                 </View>
-              </View>
+            ) : (
+                <View style={styles.container}>
+                    {showView && (
+                        <>
+                            <GridBackground
+                                position="absolute"
+                                zIndex={-5}
+                                lineColor={"black"}
+                                style={{ backgroundColor: "#f7f3eb" }}
+                            />
 
-              <View style={styles.flex3}>
-                <TouchableOpacity onPress={verifyEmail}>
-                  <Text style={styles.text2}>sign in.</Text>
-                </TouchableOpacity>
-              </View>
-            </>
-          )}
-          {showWebView && (
-            <SafeAreaView style={styles.inner}>
-              <WebView
-                style={{ flex: 1 }}
-                source={{
-                  uri: "https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1",
-                }}
-                onNavigationStateChange={onNavigationStateChange}
-              />
-            </SafeAreaView>
-          )}
+                            <View id style={styles.flex1}>
+                                <Logo fontSize={86} marginTop={"15%"} />
+                            </View>
+
+                            <View id="headingAndJoin" style={styles.flex2}>
+                                <Text id="headingText" style={styles.heading}>
+                                    Student{"\n"}Car Pooling
+                                </Text>
+                                <View id="joinFrame" style={styles.joinFrame}>
+                                    <Pressable
+                                        id="joinButton"
+                                        style={styles.joinButton}
+                                        onPress={onSignupPressed}
+                                    >
+                                        <Text style={styles.text}>Join.</Text>
+                                    </Pressable>
+                                    <View
+                                        id="joinButtonShadow"
+                                        style={[
+                                            styles.joinButton,
+                                            styles.joinButtonShadow,
+                                        ]}
+                                    ></View>
+                                </View>
+                            </View>
+
+                            <View style={styles.flex3}>
+                                <TouchableOpacity onPress={verifyEmail}>
+                                    <Text style={styles.text2}>sign in.</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </>
+                    )}
+                    {showWebView && (
+                        <SafeAreaView style={styles.inner}>
+                            <WebView
+                                style={{ flex: 1 }}
+                                source={{
+                                    uri: "https://auth.bath.ac.uk/login?service=http%3A%2F%2Fmoodle.bath.ac.uk%2Flogin%2Findex.php%3Fauthldap_skipntlmsso%3D1",
+                                }}
+                                onNavigationStateChange={
+                                    onNavigationStateChange
+                                }
+                            />
+                        </SafeAreaView>
+                    )}
+                </View>
+            )}
         </View>
-      )}
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  inner: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  flex1: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "red",
-  },
-  flex2: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    // backgroundColor: "blue",
-  },
-  flex3: {
-    flex: 1,
-    alignItems: "center",
-    // backgroundColor: "green",
-  },
-  heading: {
-    fontFamily: "syne",
-    fontSize: 40,
-    shadowColor: "#3dd37a",
-    shadowOpacity: 1,
-    shadowRadius: 2,
-    shadowOffset: {
-      height: 4,
-      width: 4,
+    container: {
+        flex: 1,
     },
-    textAlign: "center",
-  },
-  joinFrame: {
-    width: "40%",
-    height: "30%",
-    marginTop: "5%",
-    // backgroundColor: "white",
-  },
-  joinButton: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 32,
-    borderColor: "#000",
-    borderWidth: 5,
-    backgroundColor: "#3dd37a",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  joinButtonShadow: {
-    zIndex: -1,
-    position: "absolute",
-    backgroundColor: "#000",
-    top: 6,
-    left: 6,
-  },
-  text: {
-    fontSize: 36,
-    fontFamily: "atkinson",
-    color: "#ffffff",
-  },
-  text2: {
-    fontSize: 24,
-    fontFamily: "atkinson",
-    marginTop: "15%",
-  },
+    inner: {
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    flex1: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        // backgroundColor: "red",
+    },
+    flex2: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        // backgroundColor: "blue",
+    },
+    flex3: {
+        flex: 1,
+        alignItems: "center",
+        // backgroundColor: "green",
+    },
+    heading: {
+        fontFamily: "syne",
+        fontSize: 40,
+        shadowColor: "#3dd37a",
+        shadowOpacity: 1,
+        shadowRadius: 2,
+        shadowOffset: {
+            height: 4,
+            width: 4,
+        },
+        textAlign: "center",
+    },
+    joinFrame: {
+        width: "40%",
+        height: "30%",
+        marginTop: "5%",
+        // backgroundColor: "white",
+    },
+    joinButton: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 32,
+        borderColor: "#000",
+        borderWidth: 5,
+        backgroundColor: "#3dd37a",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    joinButtonShadow: {
+        zIndex: -1,
+        position: "absolute",
+        backgroundColor: "#000",
+        top: 6,
+        left: 6,
+    },
+    text: {
+        fontSize: 36,
+        fontFamily: "atkinson",
+        color: "#ffffff",
+    },
+    text2: {
+        fontSize: 24,
+        fontFamily: "atkinson",
+        marginTop: "15%",
+    },
 });
