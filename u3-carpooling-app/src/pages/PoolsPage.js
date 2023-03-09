c5b46c38721698a1dc8380da9fdd28e410768d61
wb13 <willpersonx@gmail.com>
2023-03-09 12:56:39 +0000

    Fixes chaining for username selection

--- u3-carpooling-app/src/pages/PoolsPage.js
+++ u3-carpooling-app/src/pages/PoolsPage.js
@@ -6,7 +6,14 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import RestAPI from "../hooks/Rest";
 
 const PoolsPage = () => {
-    const [singleCall, updateSingleCall] = useState(false);
+    const [callOne,   updateCallOne]   = useState(true);
+    const [recvOne,   updateRecvOne]   = useState(false);
+    const [callTwo,   updateCallTwo]   = useState(false);
+    const [recvTwo,   updateRecvTwo]   = useState(false);
+    const [callThree, updateCallThree] = useState(false);
+    const [recvThree, updateRecvThree] = useState(false);
+    const [callFour,  updateCallFour]  = useState(false);
+    const [recvFour,  updateRecvFour]  = useState(false);
 
     const onPressCancel = () => {
         console.log("Cancel Trip Pressed");
@@ -83,7 +90,8 @@ const PoolsPage = () => {
         // });
 
         // ----------------- TESTS -----------------
-        test = RestAPI(
+        
+        offer = RestAPI(
             {
                 operation: "select",
                 table: "Offer",
@@ -93,22 +101,87 @@ const PoolsPage = () => {
                 userID: null,
                 poolID: null,
             },
-            (ranAlready = singleCall)
+            (runFlag = callOne)
         )[0];
+
+		// Only run the call once
+		if (callOne == true) {
+			updateCallOne(false);
+        }
+
+        // If we received valid data, move onto the next call
+        if (recvOne == false && offer.poolID != null) {
+        	updateRecvOne(true);
+        	updateCallTwo(true);
+        }
+        
         pool = RestAPI(
             {
                 operation: "select",
                 table: "Pool",
-                poolID: test.poolID,
+                poolID: offer.poolID,
             },
             {
                 licenceID: null,
-            }
-        );
-        if (singleCall == false) updateSingleCall(true);
-    };
-    console.log(pool);
+            },
+            (runFlag = callTwo)
+        )[0];
 
+		// Only run the call once
+		if (callTwo == true) {
+			updateCallTwo(false);
+		}
+
+		// If we received valid data, move onto the next call
+		if (recvTwo == false && pool.licenceID != null) {
+			updateRecvTwo(true);
+			updateCallThree(true);
+		}
+        
+		licence = RestAPI(
+			{
+				operation: "select",
+				table: "Licence",
+				licenceID: pool.licenceID,
+			},
+			{
+				userID: null,
+			},
+			(runFlag = callThree)
+		)[0];
+
+		// Only run the call once
+		if (callThree == true) {
+			updateCallThree(false);
+		}
+
+		// If we received valid data, move onto the next call
+		if (recvThree == false && licence.userID != null) {
+			updateRecvThree(true);
+			updateCallFour(true);
+		}
+
+		user = RestAPI(
+			{
+				operation: "select",
+				table: "User",
+				userID: licence.userID,
+			},
+			{
+				name: null,
+			},
+			(runFlag = callFour)
+		)[0];
+
+		// Only run the call once
+		if (callFour == true) {
+			updateCallFour(false);
+		}
+
+		// NAME!!!!
+		if (user.name != null)
+			console.log("USER NAME:", user.name);
+    };
     initDB();
 
     const Item = ({ title }) => (
