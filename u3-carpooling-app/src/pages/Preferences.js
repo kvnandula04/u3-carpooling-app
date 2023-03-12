19f0d0279aca3c42a1db51354c47bb80a2862204
Sam Worthington <sjw233@bath.ac.uk>
2023-03-12 16:55:42 +0000

    Will - db queries

--- u3-carpooling-app/src/pages/Preferences.js
+++ u3-carpooling-app/src/pages/Preferences.js
@@ -25,17 +25,101 @@ export default function Preferences() {
     navigation.navigate("OldHomePage", {messagePage: preferences});
   };
 
-
-  const [apreferences, asetPreferences] = useState(null);
-
-  RestAPI(
-      // { operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)}
-      apreferences
-  );
-
   const handleSavePreferences = () => {
-    asetPreferences({ operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)});
-  };
+    console.log("Save Preferences"); 
+  }
+
+  const [callOne,   updateCallOne]   = useState(true);
+  const [callTwo,   updateCallTwo]   = useState(true);
+
+  const myUserID = 3;
+  var licence_table = null;
+  var pool_table = null;
+  var users_licence_id = null;
+  var users_pool_id = null;
+
+  licence_table = RestAPI(
+      {
+          operation: "select",
+          table: "Licence",
+          userID: myUserID.toString(),
+      },
+      {
+          userID: null,
+      },
+      (runFlag = callOne)
+  )[0];
+
+  // Only run the call once
+  if (callOne == true) {
+    updateCallOne(false);
+  }
+  
+  users_licence_id = licence_table.licenceID;
+
+  console.log("licence ID: ",users_licence_id);
+  
+  pool_table = RestAPI(
+    {
+      operation: "select",
+      table: "Pool",
+      licenceID: users_licence_id,
+    },
+    (runFlag = callTwo)
+  )[0];
+
+  // Only run the call once
+  if (callTwo == true) {
+  updateCallTwo(false);
+  }
+
+  users_pool_id = pool_table.poolID;
+
+  console.log("Pool ID: ",users_pool_id);
+  
+
+  // const [buttonCall, updateCall] = useState(false);
+
+  // var result = null;
+  // const myUserID = 3;
+  
+  // const handleSavePreferences = () => {
+  //   updateCall(true);
+  // };
+
+  // RestAPI(
+  // 	{ operation: "select", table: "Licence", userID: myUserID.toString()},
+	// runFlag = buttonCall
+  // );
+
+  //console.log(result[0]);
+
+
+  // const [apreferences, asetPreferences] = useState(null);
+  // const [callOne,   updateCallOne]   = useState(true);
+
+  // RestAPI(
+  //     // { operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)}
+  //     apreferences
+  // );
+
+  // const role = 1;
+  // const myUserID = 3;
+  // var result = null;
+
+
+  // const handleSavePreferences = () => {
+    //asetPreferences({ operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)});
+
+    // if(role === 1){
+    //   if (callOne === true) {
+    //     result = asetPreferences({ operation: "select", table: "Licence", userID: myUserID.toString()});
+    //     console.log(result);
+    //     updateCallOne(false);
+    //   }
+    // }
+
+  // };
 
   return (
     <View style={styles.container}>
