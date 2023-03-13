d53a031d859610fffdad7539986f79b14ecc6956
Sam Worthington <sjw233@bath.ac.uk>
2023-03-13 00:08:54 +0000

    Updated Preferenced page

--- u3-carpooling-app/src/pages/Preferences.js
+++ u3-carpooling-app/src/pages/Preferences.js
@@ -9,6 +9,7 @@ export default function Preferences() {
   
   const route = useRoute();
   const {message} = route.params;
+  console.log("Preferences page: ",message);
 
   const [preferences, setPreferences] = useState({
     location: message.location,
@@ -25,119 +26,17 @@ export default function Preferences() {
     navigation.navigate("OldHomePage", {messagePage: preferences});
   };
 
+  
+  const [apreferences, asetPreferences] = useState(null);
+
+  RestAPI(
+      // { operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)}
+      apreferences
+  );
+
   const handleSavePreferences = () => {
-    console.log("Save Preferences"); 
-  }
-
-  const [callOne,   updateCallOne]   = useState(true);
-  const [recvOne,   updateRecvOne]   = useState(false);
-  const [callTwo,   updateCallTwo]   = useState(false);
-  const [recvTwo,   updateRecvTwo]   = useState(false);
-
-  const myUserID = 3;
-  var licence_table = null;
-  var pool_table = null;
-  var users_licence_id = null;
-  var users_pool_id = null;
-
-  licence_table = RestAPI(
-    {
-      operation: "select",
-      table: "Licence",
-      userID: myUserID.toString(),
-    },
-    {
-      licenceID: null,
-    },
-    (runFlag = callOne)
-  )[0];
-
-  // Only run the call once
-  if (callOne == true) {
-    updateCallOne(false);
-  }
-
-  // If we received valid data, move onto the next call
-  if (recvOne == false && licence_table.licenceID != null) {
-  	console.log("licence ID: ", licence_table.licenceID);
-
-  	updateRecvOne(true);
-  	updateCallTwo(true);
-  }
-
-  
-  pool_table = RestAPI(
-    {
-      operation: "select",
-      table: "Pool",
-      licenceID: licence_table.licenceID,
-    },
-    {
-      poolID: null
-    },
-    (runFlag = callTwo)
-  )[0];
-
-  // Only run the call once
-  if (callTwo == true) {
-    updateCallTwo(false);
-  }
-
-  if (recvTwo == false && pool_table.poolID != null) {
-    console.log("Pool ID: ", pool_table.poolID);
-
-    // Do whatever you want with the poolID in here
-    // if you want to use it somewhere else, wrap it in an "if (pool_table.poolID != null)" clause    
-
-
-    updateRecvTwo(true);
-  }
-
-
-  
-
-  // const [buttonCall, updateCall] = useState(false);
-
-  // var result = null;
-  // const myUserID = 3;
-  
-  // const handleSavePreferences = () => {
-  //   updateCall(true);
-  // };
-
-  // RestAPI(
-  // 	{ operation: "select", table: "Licence", userID: myUserID.toString()},
-	// runFlag = buttonCall
-  // );
-
-  //console.log(result[0]);
-
-
-  // const [apreferences, asetPreferences] = useState(null);
-  // const [callOne,   updateCallOne]   = useState(true);
-
-  // RestAPI(
-  //     // { operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)}
-  //     apreferences
-  // );
-
-  // const role = 1;
-  // const myUserID = 3;
-  // var result = null;
-
-
-  // const handleSavePreferences = () => {
-    //asetPreferences({ operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)});
-
-    // if(role === 1){
-    //   if (callOne === true) {
-    //     result = asetPreferences({ operation: "select", table: "Licence", userID: myUserID.toString()});
-    //     console.log(result);
-    //     updateCallOne(false);
-    //   }
-    // }
-
-  // };
+    asetPreferences({ operation: "insert", table: "Offer", userID: "3", poolID: "3", role: "1", settings: JSON.stringify(preferences)});
+  };
 
   return (
     <View style={styles.container}>
