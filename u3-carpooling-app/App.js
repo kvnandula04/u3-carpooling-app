21fe866066d2f3e80e274a94b6cc15e2edb8f4ee
Karthik Nandula <kvnandula04@gmail.com>
2023-04-25 23:40:31 +0100

    Removes warnings

--- u3-carpooling-app/App.js
+++ u3-carpooling-app/App.js
@@ -1,16 +1,23 @@
 import { StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Navigation from "./src/navigation/Navigation";
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './globalVariables/store';
+import React from "react";
+import { Provider } from "react-redux";
+import store from "./globalVariables/store";
+import { LogBox } from "react-native";
+
+// Ignore log notification by message:
+LogBox.ignoreLogs(["Warning: ..."]);
+
+// Ignore all log notifications:
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
     <Provider store={store}>
       <GestureHandlerRootView style={{ flex: 1 }}>
-            <Navigation />
-        </GestureHandlerRootView>
+        <Navigation />
+      </GestureHandlerRootView>
     </Provider>
-    );
+  );
 }
